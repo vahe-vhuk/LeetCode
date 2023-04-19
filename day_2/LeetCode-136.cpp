d469139dcdba12d56cf94b472f6c1939c9edb472
vahe-vhuk <122445823+vahe-vhuk@users.noreply.github.com>
2023-04-19 17:45:30 +0400

    Update LeetCode-136.cpp

--- day_2/LeetCode-136.cpp
+++ day_2/LeetCode-136.cpp
@@ -7,8 +7,8 @@ complexity and use only constant extra space.
 
 Constraints:
 
-1 <= nums.length <= 3 * 104
--3 * 104 <= nums[i] <= 3 * 104
+1 <= nums.length <= 3 * 10**4
+-3 * 104 <= nums[i] <= 3 * 10**4
 Each element in the array appears twice except for one element which appears only once.
 */
 
