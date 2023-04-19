ddd47dab5d1f6ea66280e860491f270679752b75
vahe-vhuk <122445823+vahe-vhuk@users.noreply.github.com>
2023-04-19 17:44:23 +0400

    Update LeetCode-387.cpp

--- day_2/LeetCode-387.cpp
+++ day_2/LeetCode-387.cpp
@@ -2,6 +2,11 @@
 Given a string s, find the first non-repeating 
 character in it and return its index. If it does 
 not exist, return -1.
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of only lowercase English letters.
 */
 
 class Solution {
