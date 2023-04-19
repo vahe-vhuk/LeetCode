689945d6da52172fb8b7c8209152b4ec86cd1369
vahe-vhuk <122445823+vahe-vhuk@users.noreply.github.com>
2023-04-19 17:43:17 +0400

    Update LeetCode-238.cpp

--- day_1/LeetCode-238.cpp
+++ day_1/LeetCode-238.cpp
@@ -8,6 +8,12 @@ guaranteed to fit in a 32-bit integer.
 
 You must write an algorithm that runs in O(n) time 
 and without using the division operation.
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 */
 
 class Solution {
